bb38b207f6cbc632fe78e77260eb0fad4870042b
Sammy Samkough <sammysamkough@hotmail.com>
2026-01-23 10:54:50 -0500

    better organization and fix build

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,5 @@
+import type { Consumes } from "./types";
+
 export const TITLES = {
   home: "home",
   about: "about",
@@ -75,5 +77,123 @@ export const NAVIGATION: NavigationItem[] = [
     path: "/colophon",
     color: "#2d2b24ff",
     backgroundColor: "#a69b81ff",
+  },
+];
+
+export const CONSUMES: Consumes = [
+  {
+    books: [
+      {
+        name: "New American Bible (Revised Edition) (NABRE)",
+        link: "https://www.biblegateway.com/versions/New-American-Bible-Revised-Edition-NABRE-Bible/",
+        start_date: "September 2 2025",
+        end_date: null,
+        is_recent: true,
+      },
+      {
+        name: "Martyr!",
+        link: null,
+        start_date: null,
+        end_date: null,
+        is_recent: false,
+      },
+    ],
+  },
+  {
+    movies: [
+      {
+        name: "Cléo from 5 to 7",
+        link: "https://www.criterion.com/films/244-cleo-from-5-to-7",
+        start_date: "January 17 2026",
+        end_date: "January 17 2026",
+        is_recent: true,
+      },
+      {
+        name: "House of Hummingbird",
+        link: null,
+        start_date: null,
+        end_date: null,
+        is_recent: false,
+      },
+    ],
+  },
+  {
+    music: [
+      {
+        name: "Sinking Of The Titanic",
+        link: "https://gavinbryars.bandcamp.com/album/sinking-of-the-titanic",
+        start_date: null,
+        end_date: null,
+        is_recent: true,
+      },
+      {
+        name: "Taqbir",
+        link: "https://taqbir.bandcamp.com/album/taqbir",
+        start_date: null,
+        end_date: null,
+        is_recent: false,
+      },
+    ],
+  },
+  {
+    podcasts: [
+      {
+        name: "Know Your Enemy",
+        link: "https://podcasts.apple.com/us/podcast/know-your-enemy/id1462703434",
+        start_date: null,
+        end_date: null,
+        is_recent: true,
+      },
+      {
+        name: "The Dig",
+        link: "https://thedigradio.com",
+        start_date: null,
+        end_date: null,
+        is_recent: true,
+      },
+      {
+        name: "Syria Trials",
+        link: null,
+        start_date: null,
+        end_date: null,
+        is_recent: false,
+      },
+    ],
+  },
+  {
+    shows: [
+      {
+        name: "King of the Hill",
+        link: null,
+        start_date: null,
+        end_date: null,
+        is_recent: true,
+      },
+      {
+        name: "The Sopranos",
+        link: null,
+        start_date: null,
+        end_date: null,
+        is_recent: false,
+      },
+    ],
+  },
+  {
+    "video games": [
+      {
+        name: "Shovel Knight: Treasure Trove",
+        link: "https://www.yachtclubgames.com/games/shovel-knight-treasure-trove/",
+        start_date: "July 27 2025",
+        end_date: null,
+        is_recent: true,
+      },
+      {
+        name: "The Legend of Zelda: Breath of the Wild",
+        link: null,
+        start_date: null,
+        end_date: null,
+        is_recent: false,
+      },
+    ],
   },
 ];
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,3 +1,23 @@
 export interface TitleProps {
   title: string;
 }
+
+type ConsumeCategory =
+  | "books"
+  | "movies"
+  | "music"
+  | "podcasts"
+  | "shows"
+  | "video games";
+
+interface ConsumeItem {
+  name: string;
+  is_recent: boolean;
+  link?: string | null;
+  start_date?: string | null;
+  end_date?: string | null;
+}
+
+export type Consumes = Array<
+  Partial<Record<ConsumeCategory, Array<ConsumeItem>>>
+>;
