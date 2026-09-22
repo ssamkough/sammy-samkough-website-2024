c83c5f7ada5ecc6227b3e5a2b74eb51e4220fd3a
Sammy <sammysamkough@hotmail.com>
2026-09-22 00:02:29 -0400

    individual page for each new item cooks

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,7 +3,7 @@ import { defineCollection, z } from "astro:content";
 
 // 2. Define your collection(s)
 const notesCollection = defineCollection({
-  type: "content", // v2.5.0 and later
+  type: "content",
   schema: z.object({
     title: z.string(),
     description: z.string(),
@@ -13,8 +13,20 @@ const notesCollection = defineCollection({
   }),
 });
 
+const cooksCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    publishedDate: z.string(),
+    updatedDate: z.string().optional(),
+    tags: z.array(z.string()),
+  }),
+});
+
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
   notes: notesCollection,
+  cooks: cooksCollection,
 };
